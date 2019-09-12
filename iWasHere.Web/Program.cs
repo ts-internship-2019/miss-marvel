4ede1dfe041e4d1bbe1019353572c165499f8346
Corina Stoian <corina_maria.s@yahoo.com>
2019-09-12 12:18:07 +0300

    This is a test

--- iWasHere.Web/Program.cs
+++ iWasHere.Web/Program.cs
@@ -20,5 +20,7 @@ namespace iWasHere.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        //This is a test
     }
 }
