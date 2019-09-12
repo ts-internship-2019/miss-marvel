8f156acfbd25721a1e1ebb7457a6c09a056351a8
mpetre <mpetre@TS1218>
2019-09-12 12:20:36 +0300

    Test

--- iWasHere.Web/Program.cs
+++ iWasHere.Web/Program.cs
@@ -17,6 +17,7 @@ namespace iWasHere.Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
+
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
