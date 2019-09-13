6a96760b5d7c0075dc7b5ea2a6d11a167c166984
Catalin Gruia <cgruia@totalsoft.ro>
2019-09-13 16:51:32 +0300

    13.09 - Generare Modele

---
+++ iWasHere.Web/Models/LandmarkReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace iWasHere.Web.Models
+{
+    public partial class LandmarkReview
+    {
+        public int LandmarkReviewId { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewComment { get; set; }
+        public int? LandmarkId { get; set; }
+        public string UserId { get; set; }
+        public int Rating { get; set; }
+
+        public virtual Landmark Landmark { get; set; }
+    }
+}
