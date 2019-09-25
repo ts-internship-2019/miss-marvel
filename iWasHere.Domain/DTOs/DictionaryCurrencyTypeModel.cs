2ec76a98052ae69dfa5c59e0c50440f44458d01b
Corina Stoian <corina_maria.s@yahoo.com>
2019-09-25 13:15:41 +0300

    TI-73

--- iWasHere.Domain/DTOs/DictionaryCurrencyTypeModel.cs
+++ iWasHere.Domain/DTOs/DictionaryCurrencyTypeModel.cs
@@ -11,8 +11,12 @@ namespace iWasHere.Domain.DTOs
     {
      
         public int CurrencyTypeId { get; set; }
+        [Required]
+        [DisplayName("Take your time")]
         public string CurrencyName { get; set; }
+        [Required(ErrorMessage = "This field is mandatory")]
         public string CurrencyCode { get; set; }
+        [Required(ErrorMessage = "This field is mandatory")]
         public decimal CurrencyExRate { get; set; }
 
        
